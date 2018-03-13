3a160b0f54f0ddb628a0c4325210d7ce02aad877
“Maciolek <maciolek.dariusz@gmail.com>
2018-03-13 11:43:16 +0100

    corected showtasks.bat after PR

--- showtasks.bat
+++ showtasks.bat
@@ -2,15 +2,17 @@ call runcrud.bat
 if "ERRORLEVEL" == "0" goto openWebsite
 goto fail
 
+:openWebsite
+start chrome --new-window http://localhost:8080/crud/tasks
+echo Everything is OK
+goto end
+
 :fail
 echo.
 echo There were errors
 echo Cannot open file: "runcrud.bat"
 
-:openWebsite
-start chrome --new-window http://localhost:8080/crud/tasks
 :end
 echo.
-echo Everything is OK
 echo Work is finish
 
