e49cc0dd050a43bf7a1da0a2f8f7ec0859a553ff
“Maciolek <maciolek.dariusz@gmail.com>
2018-03-26 12:54:53 +0200

    added Optional
    task18.2 afert PR, added Optional

--- src/main/java/com/crud/tasks/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/client/TrelloClient.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TrelloClient {
@@ -36,13 +37,8 @@ public class TrelloClient {
         return url;
     }
 
-    public List<TrelloBoardDto> getTrelloBoards() {
-
+    public Optional<List<TrelloBoardDto>> getTrelloBoards() {
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getUrl(), TrelloBoardDto[].class);
-
-        if (boardsResponse != null) {
-            return Arrays.asList(boardsResponse);
-        }
-        return new ArrayList<>();
+        return Optional.ofNullable(Arrays.asList(boardsResponse));
     }
 }
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,6 +3,9 @@ package com.crud.tasks.controller;
 import com.crud.tasks.client.TrelloClient;
 import com.crud.tasks.domain.TrelloBoardDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +19,16 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "boards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+    public void getTrelloBoards() throws TrelloNotFoundException {
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().orElseThrow(TrelloNotFoundException::new);
         trelloBoards.stream()
                 .filter(tb -> !tb.getId().isEmpty() || !tb.getName().isEmpty())
                 .filter(tb -> tb.getName().contains("Kodilla"))
                 .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+    }
+
+    @ExceptionHandler(TrelloNotFoundException.class)
+    public ResponseEntity<Object> showMessage(TaskNotFoundException taskNotFoundException) {
+        return new ResponseEntity<>(taskNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
