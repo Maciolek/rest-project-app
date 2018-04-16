e9cfef039dc05376909fe6099f4c3b663f312231
“Maciolek <maciolek.dariusz@gmail.com>
2018-04-16 16:17:46 +0200

    changed Swagger display default setings

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,8 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/task/")
+@Api(description = "Endpoints for tasks")
+
 public class TaskController {
 
     @Autowired
@@ -23,14 +26,29 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
+
     @RequestMapping(method = RequestMethod.GET, value = "tasks")
+    @ApiOperation(value = "Returns a complete list of tasks", notes = "Retrieving collection of tasks")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 401, message = "The request requires user authentication"),
+            @ApiResponse(code = 403, message = "The server understood the request, but is refusing to fulfill it"),
+            @ApiResponse(code = 404, message = "Page not found")})
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
+    @ApiOperation(value = "Returns the task by ID", notes = "Retrieving specific task")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 401, message = "The request requires user authentication"),
+            @ApiResponse(code = 403, message = "The server understood the request, but is refusing to fulfill it"),
+            @ApiResponse(code = 404, message = "Task with given taskID does not exist")})
     @RequestMapping(method = RequestMethod.GET, value = "tasks/{taskId}")
-    @ResponseBody
-    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+    @ResponseBody()
+    public TaskDto getTask(
+            @ApiParam(required = true, name = "taskId", value = "ID of the task you want to get", defaultValue = "0")
+            @PathVariable Long taskId) throws TaskNotFoundException {
         return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
