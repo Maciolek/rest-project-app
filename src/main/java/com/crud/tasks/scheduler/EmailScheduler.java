5073b71b358badf7a05961df094c9122de5b6b96
“Maciolek <maciolek.dariusz@gmail.com>
2018-04-19 13:02:11 +0200

    task 19.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -19,13 +19,21 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-    @Scheduled(fixedDelay = 10000)
-            //(cron ="0 0 10 * * *")
-    public void sendInformationEmail (){
+    @Scheduled(fixedDelay = 30000)
+    //(cron ="0 0 10 * * *")
+
+    public void sendInformationEmail() {
+        long numberOfTasks = taskRepository.count();
+        String taskVarienty = "task";
+
+        if (numberOfTasks != 1) {
+            taskVarienty = taskVarienty + "s";
+        }
+
         emailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
-                "Currently, in database you got " + taskRepository.count() + " tasks"
+                "Currently, in database you got " + numberOfTasks + " " + taskVarienty
         ));
     }
 }
