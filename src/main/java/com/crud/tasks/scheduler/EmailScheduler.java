43ebf0ff5e8afd82eda51fcba6fcaa739e0b432f
maciolek <maciolek.dariusz@gmail.com>
2018-05-13 17:58:50 +0200

    task24.3

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -3,6 +3,7 @@ package com.crud.tasks.scheduler;
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.EmailTemplateSelector;
 import com.crud.tasks.service.SimpleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,11 +22,10 @@ public class EmailScheduler {
 
     @Scheduled(fixedDelay = 1000000)
 //            //(cron ="0 0 10 * * *")
-    public void sendInformationEmail (){
-        emailService.send(new Mail(
-                adminConfig.getAdminMail(),
-                SUBJECT,
-                "Currently, in database you got " + taskRepository.count() + " tasks"
-        ));
+    public void sendInformationEmail() {
+        emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
+                        "Currently, in database you got " + taskRepository.count() + " tasks"),
+                EmailTemplateSelector.SCHEDULED_EMAIL);
+
     }
 }
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -28,12 +28,31 @@ public class MailCreatorService {
 
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
-        context.setVariable("button", "Visit website");
+        context.setVariable("tasks_url", "https://trello.com/login/");
+        context.setVariable("button", "Visit website Trello.com");
         context.setVariable("admin_config", adminConfig);
-        context.setVariable("show_button", false);
-        context.setVariable("is_friend", true);
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", false);
         context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
+
+    public String buildScheduledMail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your task");
+        functionality.add("Provides connection with Trello account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website TASKS CRUD");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", true);
+        context.setVariable("application_functionality", functionality);
+
+        return templateEngine.process("mail/scheduled-mail", context);
+    }
 }
+
